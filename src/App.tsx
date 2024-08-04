845953ad4c606c93df906635cc0b461407dae203
mir ziauddin <mirziauddin57@gmail.com>
2024-08-04 10:31:05 +0530

    version control
    typescript version change

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(50)
-
   return (
     <>
-     
       <h1>Hello World</h1>
       <h4>This is SKB world project</h4>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
